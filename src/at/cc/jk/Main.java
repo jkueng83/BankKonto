b1039a1c90d3ec8b995dd293b1f0b0478e06f1bf
jkueng83 <johannes.kueng@gmx.at>
2020-03-30 19:51:52 +0200

    first commit

---
+++ src/at/cc/jk/Main.java
@@ -0,0 +1,24 @@
+package at.cc.jk;
+/*
+Aufgabe - Konto
+-----------------------
+Wir müssen für eine Bank ein Programm schreiben welches Aktivitäten im Zusammenhang mit dem Konto abhandeln kann.
+
+Unsere Bank hat folgende Konten im Angebot:
+
+Sparbuch
+Girokonto
+LändleGirokonto
+Kreditkonto
+
+Wir möchten einzahlen, auszahlen können, aber auch den Saldo am Konto erfahren. Je Art von Konto gibt es noch eigene
+Eigenschaften. (z.B. Zinssatz, Überziehungsrahmen, …)
+
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bank Kontos");
+    }
+}
